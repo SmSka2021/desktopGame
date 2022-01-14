87756c1f7c8fbbdb36dd3af17d5f4ab8762ccda1
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-01-14 09:40:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,163 @@
+"use strict";
+/*получаем область сообщений*/
+let message1 = document.querySelector("#messageArea_1");
+let msg1 = "Формат ввода: A1";
+message1.innerHTML = msg1;
+
+let message2 = document.querySelector("#messageArea_2");
+let msg2 = "Формат ввода: A1";
+message2.innerHTML = msg2;
+let gameOver = document.querySelector("#finish");
+
+/* рандомный генератор размещения корабля*/
+let shipsSet = new Set();
+function generateShip() {
+  while (shipsSet.size <= 18) {
+    let row = Math.floor(Math.random() * 7);
+    let column = Math.floor(Math.random() * 7);
+    let position = String(row) + String(column);
+    shipsSet.add(position);
+  }
+  return shipsSet;
+}
+generateShip();
+
+/*получаем область счёта*/
+let counter_1 = document.querySelector("#counter_1");
+let counterUser1 = 0;
+counter_1.innerHTML = counterUser1;
+
+let counter_2 = document.querySelector("#counter_2");
+let counterUser2 = 0;
+counter_2.innerHTML = counterUser2;
+
+/*Область итога игры*/
+let gameE = document.getElementById("fin");
+let counts = 0; //counterUser1 + counterUser2;
+
+//создали Map допустимых значений  c ключами A0-G6 (для проверки формата ввода)
+// и значениями 00-66(кодировка под Id) / ***Пример[A1 ; 00]****
+let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+let number = [0, 1, 2, 3, 4, 5, 6];
+let mapItem = new Map();
+for (let i = 0; i < alphabet.length; i++) {
+  for (let j = 0; j < number.length; j++) {
+    let keyMap = alphabet[i] + number[j];
+    let valueMap = number[i].toString() + number[j].toString();
+    mapItem.set(keyMap, valueMap);
+  }
+}
+/*слушаем кнопкуОГОНЬ*/ /*получаем координаты выстрела, проверяем их на валидность и очищаем окно ввода*/
+let fireBtn1 = document.querySelector("#fireButton_1");
+let fireBtn2 = document.querySelector("#fireButton_2");
+let input1 = document.querySelector("#input_1");
+let input2 = document.querySelector("#input_2");
+let val1 = "";
+let val2 = "";
+
+fireBtn1.addEventListener("click", valueFire1);
+function valueFire1() {
+  let valueFire1 = input1.value.toUpperCase();
+  if (mapItem.has(valueFire1)) {
+    val1 = mapItem.get(valueFire1);
+    mapItem.delete(valueFire1);
+    input1.value = "";
+
+    if (shipsSet.has(val1)) {
+      let resultat = document.getElementById([val1]);
+      resultat.setAttribute("class", "ship");
+      message1.innerHTML = "Попал!";
+      counterUser1 = counterUser1 + 1;
+      counts = counts + 1;
+      let audio2 = new Audio(); // Создаём новый элемент Audio
+      audio2.src = "./audio/vyistrel.mp3"; 
+      audio2.autoplay = true;
+
+      if (counts === 19) {
+        if (counterUser1 > counterUser2) {
+          gameE.innerHTML = "ПОБЕДА!!! Игрок №1- чемпион!";
+        } else {
+          gameE.innerHTML = "ПОБЕДА!!! Игрок №2- чемпион!";
+        }
+        setInterval(() => (gameE.hidden = !gameE.hidden), 1000);
+        let audio = new Audio(); 
+        audio.src = "./audio/fanfary.mp3"; 
+        audio.autoplay = true;
+      }
+      counter_1.innerHTML = counterUser1;
+      return counterUser1;
+    } else {
+      let audio2 = new Audio(); 
+      audio2.src = "./audio/water.mp3"; 
+      audio2.autoplay = true;
+
+      let resultat = document.getElementById([val1]);
+      resultat.setAttribute("class", "fish");
+      message1.innerHTML = "Мимо";
+      return counterUser1;
+    }
+  } else {
+    alert("Проверь формат ввода");
+  }
+}
+
+fireBtn2.addEventListener("click", valueFire2);
+function valueFire2() {
+  let valueFire2 = input2.value.toUpperCase();
+  if (mapItem.has(valueFire2)) {
+    val2 = mapItem.get(valueFire2);
+    mapItem.delete(valueFire2);
+    input2.value = "";
+
+    if (shipsSet.has(val2)) {
+      let resultat = document.getElementById([val2]);
+      resultat.setAttribute("class", "ship");
+      message2.innerHTML = "Попал!";
+      counterUser2 = counterUser2 + 1;
+      counts = counts + 1;
+      let audio2 = new Audio(); 
+      audio2.src = "./audio/vyistrel.mp3"; 
+      audio2.autoplay = true;
+      if (counts === 19) {
+        if (counterUser1 > counterUser2) {
+          gameE.innerHTML = "ПОБЕДА!!! Игрок №1- чемпион!";
+        } else {
+          gameE.innerHTML = "ПОБЕДА!!! Игрок №2- чемпион!";
+        }
+        setInterval(() => (gameE.hidden = !gameE.hidden), 1000);
+        let audio = new Audio(); 
+        audio.src = "./audio/fanfary.mp3"; 
+        audio.autoplay = true;
+      }
+      counter_2.innerHTML = counterUser2;
+      return counterUser2;
+    } else {
+      let resultat = document.getElementById([val2]);
+      resultat.setAttribute("class", "fish");
+      message2.innerHTML = "Мимо";
+      let audio2 = new Audio(); 
+      audio2.src = "./audio/water.mp3"; 
+      audio2.autoplay = true;
+      return counterUser2;
+    }
+  } else {
+    alert("Проверь формат ввода");
+  }
+}
+
+/*нажатие Enter*/
+input1.addEventListener("keypress", validationPress1);
+input2.addEventListener("keypress", validationPress2);
+
+function validationPress1(event) {
+  if (event.keyCode == 13) {
+    valueFire1();
+  }
+}
+function validationPress2(event) {
+  if (event.keyCode == 13) {
+    valueFire2();
+  }
+}
+
+window.onload = generateShip();
